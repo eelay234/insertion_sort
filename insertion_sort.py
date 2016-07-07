dd92b2d32ae71b2368e77fd19f9bdc935858fde0
emnie <eelay234@gmail.com>
2016-07-07 10:09:08 -0700

    insertion_sort.py added

---
+++ insertion_sort.py
@@ -0,0 +1,23 @@
+import random
+import math
+import time
+
+def insertion_sort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+	i = j - 1
+	while i >= 0 and A[i] > key:
+	    A[i+1] = A[i]
+	    i -= 1
+	A[i+1] = key
+
+a = []
+for i in range(1, 101):
+    a.append(int(random.random()*10000))
+
+begin = time.time()
+insertion_sort(a)
+end = time.time()
+
+print a
+print "It took %.2f seconds" % (end - begin)
